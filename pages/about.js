f1dfe557500989c68081a2a2bec8c929a80a98de
FlashBangKev <kevin@getkev.co.uk>
2021-10-28 22:38:42 +0100

    way point save

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head'
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About us</title>
+                <meta name="keywords" content="software engineering, about us"/>
+            </Head>
+            <h1>This is the about page</h1>
+        </div>
+    )
+}
+
+export default about
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import Head from 'next/head'
 export default function Home() {
   return (
    <div>
+     <Head>
+       <title>This is a test</title>
+       <meta name="keywords" content="web development, programming, bullshit" />
+     </Head>
      <h1>Welcome to Next</h1>
    </div>
   )
